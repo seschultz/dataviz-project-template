6625d64dce962e3349e0fc49ca4b0cfa4665cb77
Sarah Schultz <sschultz@SSCHULTZ-MAC1.ML.CS.CMU.EDU>
2017-11-05 19:21:38 -0500

    updated bundle

--- dist/bundle.js
+++ dist/bundle.js
@@ -67,24 +67,31 @@
 /* 0 */
 /***/ (function(module, exports) {
 
-d3.csv('data/diabetes2015.csv', data => {
-        
-   // Assemble a summary string.
-   const summary = [
-     'Data table summary: ',
-      data.length + ' rows',
-      data.columns.length + ' columns',
-      Math.round(d3.csvFormat(data).length / 1024) + ' kB',
-      '\nSummary of each column:',
-      dl.format.summary(data)
-    ].join('\n');
-        
-    // Show the summary string on the page.
-    d3.select('body').append('pre').text(summary);
-        
-    // Log the summary to the console.
-    console.log(summary);
-});
+var projection = d3
+		.geoMercator() 
+		.scale(37000)	
+		.rotate([-0.25, 0.25, 0]) 
+		.center([-80.4, 40.5]); 
+	
+	var path = d3.geoPath().projection(projection);　
+	
+	var map = d3.select("body")
+		.append("svg")
+		.attr("width", 960)
+		.attr("height", 500); 
+	
+	d3.json("data/Allegheny_County_Census_Tracts_2016.geojson", drawMaps);
+	
+	function drawMaps(geojson) {
+		map.selectAll("path")
+			.data(geojson.features)
+			.enter()
+			.append("path")
+			.attr("d", path)  
+			.attr("fill", "green")
+			.attr("fill-opacity", 0.6)
+			.attr("stroke", "#222");    
+	}
 
 
 /***/ })
